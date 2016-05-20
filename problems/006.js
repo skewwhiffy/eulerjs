972e81cda351f4777b30fe7428a34fb55e375928
Kenny Hung <kennysubs+github@outlook.com>
2016-05-20 21:32:57 +0100

    Problem 6

---
+++ problems/006.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/*
+
+Sum square difference
+Problem 6
+The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+
+ */
+
+let expect = require("chai").expect;
+let PrimeFactors = require("../utils/primeFactors");
+
+describe.only("Problem 6", function() {
+    let solution = upperLimit => {
+        let naturals = [];
+        for (let i = 1; i <= upperLimit; i++) {
+            naturals.push(i);
+        }
+        let sumOfSquares = naturals.map(n => n * n).reduce((p, c) => p + c, 0);
+        let squareOfSum = naturals.reduce((p, c) => p + c, 0);
+        squareOfSum *= squareOfSum;
+        return squareOfSum - sumOfSquares;
+    }
+
+    it("Test data", () => {
+        let testAnswer = solution(10);
+        expect(testAnswer).to.equal(2640);
+    })
+
+    it("Solution: " + solution(100), () => {})
+})
