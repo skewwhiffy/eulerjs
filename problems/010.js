0edca8679ec8296a6858e814e70703237244aa3a
Kenny Hung <kennysubs+github@outlook.com>
2016-05-21 15:21:53 +0100

    Problem 10

---
+++ problems/010.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/*
+
+Summation of primes
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+ */
+
+let expect = require("chai").expect;
+let Prime = require("../utils/prime");
+
+describe("Problem 10", function() {
+    let prime = new Prime();
+
+    let solution = upperLimit => {
+        let sum = 0;
+        let count = 0;
+        while(true) {
+            let current = prime.prime(count);
+            if (current >= upperLimit) return sum;
+            sum += current;
+            count++;
+        }
+    }
+
+    it("Test data", () => {
+        let testResult = solution(10);
+        expect(testResult).to.equal(17);
+    })
+
+    it("Solution: " + solution(2000000), () => {})
+})
